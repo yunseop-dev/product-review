16d6c072a3bd480f1940e253dff04c42336983a9
Yunseop Kim <yunseop.kim@liveklass.com>
2025-03-07 16:24:44 +0900

    feat: Make site title clickable with home page link

--- widgets/layout/ui/SiteHeader.tsx
+++ widgets/layout/ui/SiteHeader.tsx
@@ -1,9 +1,11 @@
 import AuthLinks from "@/features/auth/ui/AuthLinks";
-
+import Link from "next/link";
 export default function SiteHeader() {
   return (
     <header className="flex justify-between items-center mb-12">
-      <h1 className="text-3xl font-bold">제품 리뷰 사이트</h1>
+      <h1 className="text-3xl font-bold">
+        <Link href="/">제품 리뷰 사이트</Link>
+      </h1>
       <AuthLinks />
     </header>
   );
